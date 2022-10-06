32e17b67bf4e9c6bf9954188f20b89e1b818dbf5
olol1631 <ol.ol1631@gmail.com>
2022-10-06 17:56:34 +0300

    add swiper

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.slider', { 
+    slidesPerView: 3,
+    spaceBetween: 50, 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+  });
