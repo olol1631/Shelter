283b89b9ef343730b2c1ac891909fa88668143ae
olol1631 <ol.ol1631@gmail.com>
2022-10-06 21:40:17 +0300

    add burger-menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// burger-menu
+let burger = document.querySelector('.menu__burger');
+let menuList = document.querySelector('.menu__list')
+burger.addEventListener('click', () => {
+  burger.classList.toggle('open')
+  menuList.classList.toggle('open')  
+  document.querySelector('body').classList.toggle('lock') 
+})
+document.querySelector('.bg-mobile-menu').addEventListener('click', ()=>{
+  burger.classList.remove('open')
+  menuList.classList.remove('open')  
+  document.querySelector('body').classList.remove('lock') 
+});
+
+menuList.addEventListener('click', (e)=>{
+  if(e.target !== menuList){
+    burger.classList.remove('open')
+    menuList.classList.remove('open')  
+    document.querySelector('body').classList.remove('lock') 
+  }
+})
+
 const swiper = new Swiper('.slider', { 
     slidesPerView: 3,
     spaceBetween: 50, 
@@ -6,5 +28,15 @@ const swiper = new Swiper('.slider', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-   
+    breakpoints: { 
+        320: {
+          slidesPerView: 1,  
+        }, 
+        680: {
+          slidesPerView: 2, 
+        }, 
+        900: {
+          slidesPerView: 3, 
+        }
+      }
   });
